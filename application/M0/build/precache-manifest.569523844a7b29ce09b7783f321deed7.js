3bcaf76edad647e86403b512cd86c1f8021854d5
dangle730 <dang.le730@gmail.com>
2020-02-13 14:27:57 -0800

    home

---
+++ application/M0/build/precache-manifest.569523844a7b29ce09b7783f321deed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0ef46454224de14df4d3e8a4e10813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a7afd641c4e3f1d8f8",
+    "url": "/static/css/main.5f9e14b6.chunk.css"
+  },
+  {
+    "revision": "d72396758212bd3fd6d7",
+    "url": "/static/js/2.1ab88e41.chunk.js"
+  },
+  {
+    "revision": "67a7afd641c4e3f1d8f8",
+    "url": "/static/js/main.492decc0.chunk.js"
+  },
+  {
+    "revision": "6c9053e49ccee1446dd0",
+    "url": "/static/js/runtime-main.18b8198f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
