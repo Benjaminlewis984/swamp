e5fd07d50e1f0be9fb7085f4413bfbfb45ad102f
weerachai poorakkiat <wpoorakkiat@mail.sfsu.edu>
2020-02-13 15:24:22 -0800

    Joe push

--- application/M0/src/App.js
+++ application/M0/src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 import Home from './pages/Home';
 import Dang from './pages/Dang';
 import William from './pages/William';
+import Joe from './pages/Joe';
 // import Ben from './pages/Ben';
 // etc..
 
@@ -18,13 +19,19 @@ const App = () => {
           </div>
           <div>
             <Link to="/dang">Dang</Link>
+          </div>
+          <div>
             <Link to="/william">William</Link>
+          </div>
+          <div>
+            <Link to="/Joe">Weerachai</Link>
           </div>
         </div>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/dang" component={Dang} />
           <Route exact path="/william" component={William} />
+          <Route exact path="/Joe" component={Joe} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ application/M0/src/pages/Joe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import joe from "../imgs/Joe.jpg";
+
+const Joe = () => {
+    return (
+        <div>
+            <h1>Weerachai Poorakkiat</h1>
+            <img class="profile" src={joe} />
+            <p>Hello Hungry People :)</p>
+        </div>
+    );
+};
+
+export default Joe;
