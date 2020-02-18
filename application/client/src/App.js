2a3dd2094bedd2e6d25be8cf8e5e8a56c22632fe
dangle730 <dang.le730@gmail.com>
2020-02-18 01:33:16 -0800

    about team organized

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 
 import Home from './pages/Home';
+import Team from './pages/Team';
 import Dang from './pages/Dang';
 import William from './pages/William';
 import Joe from './pages/Joe';
@@ -19,30 +20,16 @@ const App = () => {
             <Link to="/">Home</Link>
           </div>
           <div>
-            <Link to="/dang">Dang</Link>
-          </div>
-          <div>
-            <Link to="/william">William</Link>
-          </div>
-          <div>
-            <Link to="/Joe">Weerachai</Link>
-          </div>
-          <div>
-            <Link to="/Kevin">Kevin</Link>
-          </div>
-          <div>
-            <Link to="/ben">Ben</Link>
-          </div>
-          <div>
-            <Link to="/onu">Onu</Link>
+            <Link to="/about">About</Link>
           </div>
         </div>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={Team} />
           <Route exact path="/dang" component={Dang} />
-          <Route exact path="/william" component={William} />
-          <Route exact path="/Joe" component={Joe} />
-          <Route exact path="/Kevin" component={Kevin} />
+          <Route exact path="/will" component={William} />
+          <Route exact path="/joe" component={Joe} />
+          <Route exact path="/kevin" component={Kevin} />
           <Route exact path="/ben" component={Ben} />
           <Route exact path="/onu" component={Onu} />
         </Switch>
---
+++ application/client/src/pages/Dang.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import dang from "../imgs/dang.jpg";
+
+const Dang = () => {
+    return (
+        <div class="container">
+            <h1>Dang Le</h1>
+            <img class="profile" src={dang} />
+            <h4>Senior at San Francisco State University</h4>
+            <p class="box-lim">
+                Born in Saigon, raised in California <br /> <br />
+                Majoring in computer science with dreams of becoming a game developer. 
+                Outside of school, I proudly work at the restaurant <i>MAMA</i> in Oakland.
+                On my free time I enjoy hiking, exploring music, and playing video games.
+            </p>
+        </div>
+    );
+};
+
+export default Dang;
