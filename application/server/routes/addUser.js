0693363ed907e300fb85cd151df88c3ad86a44b8
Kevin Huynh <anmevee@yahoo.com>
2020-02-21 00:10:47 -0800

    fixed sfsu email regex, but unable to check if it works

--- application/server/routes/addUser.js
+++ application/server/routes/addUser.js
@@ -8,23 +8,28 @@ router.get('/addUser/:username/:password/:email', function(req, res, next) {
   var password = req.params.password;
   var email = req.params.email;
   
-  if (username == username.replace(/[^A-Za-z0-9]/gi,'') && email == email.replace(/[^A-Za-z0-9]*@mail\.sfsu\.edu/gi,'')) {
+  if (username == username.replace(/[^A-Za-z0-9]/gi,'') && email == email.replace(/[^A-Za-z0-9]/gi,'')) {
+    if(/[A-Za-z0-9]*[@mail\.sfsu\.edu]/gi.test(email)) {
       userManager.getUserFromUsername(username, (usernameResult) => {
-      if (usernameResult == undefined) {
-        userManager.getUserFromEmail(email, (emailResult) => {
-          if (emailResult == undefined) {
-            userManager.addUser(username, password, email);
-            res.send("Added user!");
-          }
-          else {
-            res.send("User already exists!");
-          }
-        });
-      }
-      else {
-        res.send("User already exists!");
-      }
-    });
+        if (usernameResult == undefined) {
+          userManager.getUserFromEmail(email, (emailResult) => {
+            if (emailResult == undefined) {
+              userManager.addUser(username, password, email);
+              res.send("Added user!");
+            }
+            else {
+              res.send("Email already exists!");
+            }
+          });
+        }
+        else {
+          res.send("Username already exists!");
+        }
+      });
+    }
+    else {
+      res.send("Invalid Email");
+    }
   }
   else {
     res.send("Invalid information!");
