76569fd4324e845f301c66a7c9af596ecb12fa7d
William Lew <WilliamMilesLew@gmail.com>
2020-02-27 17:42:46 -0800

    added: new commands for media manager

--- application/server/database/media-manager.js
+++ application/server/database/media-manager.js
@@ -4,3 +4,18 @@ exports.addMedia = (title, description, preview_path, raw_path) => {
   var queryString = "('" + title + "', '" + description + "', '" + preview_path + "', '" + raw_path + "');";
   databaseManager.queryDatabase("INSERT INTO media(title, description, preview_path, raw_path) VALUES " + queryString, (result) => {});
 }
+
+exports.deleteMedia = (title) => {
+  var queryString = "'" + title + "';";
+  databaseManager.queryDatabase("DELETE FROM media WHERE title = " + queryString, (result) => {});
+}
+
+exports.approveMedia = (title) => {
+  var queryTitle = "'" + title + "';";
+  databaseManager.queryDatabase("UPDATE media SET status = 'approved' WHERE title = " + queryTitle, (result) => {});
+}
+
+exports.rejectMedia = (title) => {
+  var queryTitle = "'" + title + "';";
+  databaseManager.queryDatabase("UPDATE media SET status = 'rejected' WHERE title = " + queryTitle, (result) => {});
+}
