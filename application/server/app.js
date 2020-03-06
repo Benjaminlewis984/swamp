95becfa6b3520981ef62dff71a94be75bd79d24f
William Lew <WilliamMilesLew@gmail.com>
2020-03-05 17:39:16 -0800

    cleaned: made all tables center aligned

--- application/server/app.js
+++ application/server/app.js
@@ -33,7 +33,7 @@ app.use(session({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'media/preview')));
+app.use(express.static(path.join(__dirname, 'media')));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(fileUpload());
