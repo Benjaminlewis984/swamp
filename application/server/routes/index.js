2680c9fa09dcac56a76e4d5f1406ff9ef41eb64b
William Lew <WilliamMilesLew@gmail.com>
2020-03-09 22:26:36 -0700

    added: user specific controls

--- application/server/routes/index.js
+++ application/server/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Database Dashboard', user: req.user });
+  console.log(req.user);
 });
 
 module.exports = router;
