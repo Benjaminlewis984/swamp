0e1201bdf443d4f399a82153c91c177ceeecc599
William Lew <WilliamMilesLew@gmail.com>
2020-03-09 23:43:21 -0700

    added: basic browse page

--- application/server/routes/browseMedia.js
+++ application/server/routes/browseMedia.js
@@ -1,11 +1,19 @@
 var mediaManager = require('../database/media-manager.js');
+var userManager = require('../database/user-manager.js');
 var express = require('express');
 var router = express.Router();
 
 router.get('/browse', (req, res, next) => {
   mediaManager.getMediaApproved(25, 0, (results) => {
-    res.render('browse', {results: results});
-    console.log(results);
+    results.forEach((result, idx) => {
+        userManager.getUserFromID(result.author_id, (user) => {
+          result["author_username"] = user[0].username;
+
+          if (idx == results.length - 1) {
+            res.render('browse', {results: results});
+          }
+        });
+      });
   });
 });
 
