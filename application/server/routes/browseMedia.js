c3b5181b3625077695b543f107694f766ff02bc2
William Lew <WilliamMilesLew@gmail.com>
2020-03-10 10:20:41 -0700

    fixed: authors not appearing after changing categories

--- application/server/routes/browseMedia.js
+++ application/server/routes/browseMedia.js
@@ -20,12 +20,28 @@ router.get('/browse', (req, res, next) => {
 router.post('/browse', (req, res, next) => {
   if (req.body.category == 'all') {
     mediaManager.getMediaApproved(25, 0, (results) => {
-      res.render('browse', {results: results});
+      results.forEach((result, idx) => {
+        userManager.getUserFromID(result.author_id, (user) => {
+          result["author_username"] = user[0].username;
+
+          if (idx == results.length - 1) {
+            res.render('browse', {results: results});
+          }
+        });
+      });
     });
   }
   else {
     mediaManager.getMediaApprovedCategory(25, 0, req.body.category, (results) => {
-      res.render('browse', {results: results});
+      results.forEach((result, idx) => {
+        userManager.getUserFromID(result.author_id, (user) => {
+          result["author_username"] = user[0].username;
+
+          if (idx == results.length - 1) {
+            res.render('browse', {results: results});
+          }
+        });
+      });
     });
   }
 });
