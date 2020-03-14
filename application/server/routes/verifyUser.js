96413692b9f40e0c94f734e0d1f72ba5ad38eb18
William Lew <WilliamMilesLew@gmail.com>
2020-03-13 19:05:42 -0700

    updated: verifyUser.js to match standards

--- application/server/routes/verifyUser.js
+++ application/server/routes/verifyUser.js
@@ -5,8 +5,17 @@ var passport = require('passport');
 var pp_config = require('../modules/passport-config');
 pp_config(passport);
 
+var request = require('request');
+
 router.get('/login', alreadyAuth, (req, res, next) => {
-  res.render('login', { title: 'Sign in'});
+  if (req.query.username != undefined) {
+    request.post('http://0.0.0.0:3001/login', {json: req.query}, (error, response, body) => {
+      res.render('index', { title: 'Database Dashboard', user: body.user });
+    });
+  }
+  else {
+    res.render('login', { title: 'Sign in'});
+  }
 });
   
 /**
@@ -16,7 +25,7 @@ router.get('/login', alreadyAuth, (req, res, next) => {
  */
 router.post('/login', passport.authenticate('local'), (req, res, next) => {
   res.status(200);
-  res.send({success: "true"});
+  res.send({success: "true", user: req.user});
 });
 
 /**
