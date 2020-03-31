45511b8b51c48ce4182391065eacc8724f46ed95
dangle730 <dang.le730@gmail.com>
2020-03-31 14:30:40 -0700

    button does not work

--- application/client/src/pages/Browse.js
+++ application/client/src/pages/Browse.js
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Label, Form, FormGroup, Input, Alert } from 'reactstrap';
-import { Button } from 'react-bootstrap';
 import axios from 'axios';
 
 const Browse = () => {
   const [query, setQuery] = useState('');
   const [result, setResult] = useState([]);
 
-  function searchTitle(e) {
-        axios.post('http://0.0.0.0:3001/browse', {
-            
-        }).then((res) => {
-            setResult(res.data)
-        }).catch(err => console.log(err));
-    }
+  const searchByTitle = () => {
+    console.log('Button click')
+
+    axios.post('http://localhost:3001/browse', {
+      category: 'all',
+      search: query,
+    }).then((res) => {
+        console.log('TEST');
+        setResult(res.data.results);
+        setQuery('');
+    }).catch(err => console.log(err));
+  }
 
   return (
     <div>
       <h1>Browse</h1>
-      <Input type='text' value={query} onChange={e=>setQuery(e.target.value)} placeholder='Item Title' />
-      <Button onClick={searchTitle} block>Search</Button>
+      <Input type='text' value={query} onChange={e=>setQuery(e.target.value)} placeholder='Search by title..' />
+      <button onClick={searchByTitle}>Search</button>
+      <h2>{result[0]}</h2>
     </div>
   );
 };
