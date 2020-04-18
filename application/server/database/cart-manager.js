3b0a2e58025010d160b321bdc68d2e3cc0ac45e0
Kevin Huynh <anmevee@yahoo.com>
2020-04-17 17:00:45 -0700

    implemented shopping cart dashboard and fixed shopping cart

--- application/server/database/cart-manager.js
+++ application/server/database/cart-manager.js
@@ -5,7 +5,7 @@ exports.addToCart = (m_id, acc_id) => {
   let reg_id;
   let price;
 
-  databaseManager.queryDatabase(`SELECT approved.approved_id, media.price FROM \`media content\` media, \`approved media\` approved WHERE approved.m_id = media.m_id;`, (result) => {
+  databaseManager.queryDatabase(`SELECT approved.approved_id, media.price FROM \`media content\` media, \`approved media\` approved WHERE approved.m_id = media.m_id AND media.m_id = ${m_id};`, (result) => {
     approved_id = result[0]['approved_id'];
     price = result[0]['price'];
     databaseManager.queryDatabase(`SELECT registered.reg_id FROM \`registered users\` registered INNER JOIN accounts ON registered.acc_id = accounts.acc_id WHERE accounts.acc_id = ${acc_id};`, (result) => {
@@ -15,14 +15,18 @@ exports.addToCart = (m_id, acc_id) => {
   });
 }
 
-exports.getFromCartByID = (reg_id, action) => {
-  databaseManager.queryDatabase(`SELECT COUNT(*) FROM \`shopping cart\`;`, (count) => {
-    if(count[0]['COUNT(*)'] > 0) {
-      databaseManager.queryDatabase(`SELECT * FROM \`shopping cart\` WHERE reg_id = ${reg_id};`, (result) => {
+exports.getFromCartByID = (acc_id, action) => {
+  let reg_id;
+  databaseManager.queryDatabase(`SELECT registered.reg_id FROM \`registered users\` registered INNER JOIN accounts ON registered.acc_id = accounts.acc_id WHERE accounts.acc_id = ${acc_id};`, (result) => {
+    reg_id = result[0]['reg_id'];
+    databaseManager.queryDatabase(`SELECT COUNT(*) FROM \`shopping cart\` WHERE reg_id = ${reg_id};`, (count) => {
+      if(count[0]['COUNT(*)'] > 0) {
+        databaseManager.queryDatabase(`SELECT * FROM \`media content\` media, \`shopping cart\` cart, \`approved media\` approved WHERE cart.approved_id = approved.approved_id AND approved.m_id = media.m_id AND cart.reg_id = ${reg_id};`, (result) => {
           action(result);
-      });
-    } else {
-        action(undefined);
-    }
+        });
+      } else {
+          action(undefined);
+      }
+    })
   })
 }
--- application/server/routes/ShoppingCart/addToCart.js
+++ application/server/routes/ShoppingCart/addToCart.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const cartManager = require('../../database/cart-manager.js');
+const userManager = require('../../database/user-manager.js');
 
 router.get('/cart', (req, res, next) => {
-  cartManager.getFromCartByID(req.user.reg_id, (results) => {
+  cartManager.getFromCartByID(req.user.acc_id, (results) => {
     if(results == undefined) {
-        res.send('There are no listings in your shopping cart at this time.');
+      res.send('There are no listings in your shopping cart at this time.');
     } else {
       results.forEach((result, idx) => {
-        console.log(result);
+         userManager.getUserFromID(result.acc_id, (user) => {
+           result["author_username"] = user[0].username;
+  
+           if (idx == results.length - 1) {
+             res.render('cart', {results: results});
+           }
+         });
       })
     }
   });
 })
 
 router.post('/cart', (req, res, next) => {
-  cartManager.addToCart(req.body.approved_id, req.user.acc_id);
+  console.log(req.body)
+  cartManager.addToCart(req.body.m_id, req.user.acc_id);
   res.status(200);
   res.send({success: "true"});
 })
