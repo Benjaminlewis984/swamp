60a953c8f61fa357b70630f3d456e3f510ab8f76
weerachai poorakkiat <wpoorakkiat@mail.sfsu.edu>
2020-04-23 22:37:01 -0700

    Work on Sign up and Log in

--- application/client/src/index.js
+++ application/client/src/index.js
@@ -7,10 +7,10 @@ import { ProductProvider } from './context';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './redux/reducers/rootReducer';
-import { createStore } from 'redux';
-import searchReducer, {searchResults} from './redux/reducers/searchReducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(thunk));
 
 ReactDOM.render(
     <ProductProvider>
--- application/client/src/redux/actions/signupAction.js
+++ application/client/src/redux/actions/signupAction.js
@@ -20,24 +20,23 @@ export const signup = () => (dispatchEvent, getState) => {
     console.log('Sign up Function !!!');
     const username = getState().signupReducer.username;
     const password = getState().signupReducer.password;
-    const notes = getState().notesReducer.notes;
+    
     if(username.length > 0 && password.length > 0){
         const axios = require("axios");
         const body = {
             username: `${username}`,
             password: `${password}`,
-            notes: `${notes}` 
         };
-        const url =(`http://18.191.184.143:3001/signup?username=${username}&password=${password}`)
+        const url =(`http://18.191.184.143:3001/register?username=${username}&password=${password}`)
         fetch(url)
         .then((data) => {
             console.log(data);
-            if(!data.valid){
-                dispatchEvent(setLoadingState('error'));
-            }
-            else{
-                axios.post(`http://18.191.184.143:3001/signup?username=${username}&password=${password}`,body) 
-            }
+            // if(!data.valid){
+            //     dispatchEvent(setLoadingState('error'));
+            // }
+            // else{
+                axios.post(`http://18.191.184.143:3001/register?username=${username}&password=${password}`,body) 
+            //}
         })
 }
 };
