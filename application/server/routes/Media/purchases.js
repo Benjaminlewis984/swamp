85968f00986689c4506cae6353ea9a3eafd26c0f
William Lew <WilliamMilesLew@gmail.com>
2020-04-27 17:14:13 -0700

    finished: my purchases endpoints

--- application/server/routes/Media/purchases.js
+++ application/server/routes/Media/purchases.js
@@ -1,12 +1,27 @@
 const mediaManager = require('../../database/media-manager.js');
+const userManager = require('../../database/user-manager.js');
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 router.get('/purchases', async (req, res, next) => {
-  let accountID = req.user.acc_id;
+  request.post('http://0.0.0.0:3001/purchases', {json: {query: req.query, user: req.user}}, (error, response, body) => {
+    res.render('purchases', {results: body.results});
+  });
+});
+
+router.post('/purchases', async (req, res, next) => {
+  let accountID = req.body.user.acc_id;
   const results = await mediaManager.getPurchases(25, 0, accountID);
 
-  res.render('purchases');
+  results.forEach(async (result, idx) => {
+    const userResult = await userManager.getUserFromID(result.acc_id);
+    result['author_username'] = userResult[0].username;
+    result.bought = 'false';
+    result.preview_path = result.preview_path.substr(result.preview_path.indexOf('preview/') + 8);
+
+    if(idx == results.length - 1) { return res.status(200).send({success: true, results: results}); }
+  });
 });
 
 module.exports = router;
