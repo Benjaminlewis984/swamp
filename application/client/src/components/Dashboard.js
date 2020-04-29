271f79efbefc92a24ee31a27beba92ba9872e37e
Onubulachi Wami <oawami@gmail.com>
2020-04-28 19:35:58 -0700

    Added profile page, still blank

---
+++ application/client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import logo from '../imgs/gator.png';
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+           <div>Hello from Dashboard</div>
+        
+        )
+    }
+}
