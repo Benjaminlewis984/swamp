54ae23b51b26e9e5be46a5388a716fd06b01d82a
Kevin Huynh <anmevee@yahoo.com>
2020-05-03 19:32:12 -0700

    GET requests to /profile now requires username for scalability

--- application/server/routes/Users/userProfile.js
+++ application/server/routes/Users/userProfile.js
@@ -6,11 +6,25 @@ const userManager = require('../../database/user-manager.js');
 
 const profilePath = path.join(__dirname, '../../media/public/profile');
 
-router.get('/profile', (req, res) => {
-
-  res.render('profile', {path: req.user.profile_path.substring(8)});
+/**
+ * Renders the profile picture of a user
+ * 
+ * @param req.query.username: Username of the profile picture that is viewed
+ * @return: Path of the profile picture in question
+ */
+router.get('/profile', async (req, res) => {
+  const username = req.query.username;
+  const user = await userManager.getUserFromUsername(username);
+  res.render('profile', {path: user.profile_path.substring(8)});
 });
 
+/**
+ * Endpoint to change user's profile picture. Takes a file and
+ * stores in it the filesystem, while updating the path in the database.
+ * 
+ * @param req.files.file: The image that user wants as profile picture.
+ * @return: "true" or "false" depending if user successfully changed their profile picture.
+ */
 router.post('/profile', (req, res) => {
   const file = req.files.file;
 
@@ -34,7 +48,7 @@ router.post('/profile', (req, res) => {
     const profilePath = "profile/" + dateString + fileNumber + fileExtension;
     file.mv('./media/public/' + profilePath, (err) => {
       userManager.updateUserProfilePicture(profilePath, req.user.acc_id);
-      res.status(200).send({success: "true"});
+      return res.status(200).send({success: "true"});
     });
   });
 });
