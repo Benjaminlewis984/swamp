f288a9c2d50df5470a1c85ff9aa02a3eb60ede70
Kevin Huynh <anmevee@yahoo.com>
2020-05-21 14:33:31 -0700

    added spacing for dashboard

--- application/client/src/components/Dashboard.js
+++ application/client/src/components/Dashboard.js
@@ -298,6 +298,8 @@ const Dashboard = () => {
 								{userInfo.first_name} {userInfo.last_name}
 							</h5>
 							<h6>Status: Student</h6>
+							<br></br>
+							<br></br>
 							<ul class="nav nav-tabs" id="myTab" role="tablist">
 								<li class="nav-item">
 									<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true" onClick={showPosts}>Posts</a>
